b7c244c68beec54a716cc600eae42e6505b38b66
Marissa Dietz <User@MacBook-Pro.local>
2014-05-01 13:35:48 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Marissa Dietz on 5/1/14.
+//  Copyright (c) 2014 CS193P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Marissa Dietz on 5/1/14.
+//  Copyright (c) 2014 CS193P. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
